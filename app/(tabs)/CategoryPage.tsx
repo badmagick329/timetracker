0dae1658eb2d2d82e2b0d2dddf5407ba40fbb460
badmagick329 <uzairfarooqi@gmail.com>
2025-05-06 23:43:27 +0100

    feat: add modal confirmation on category deletion

--- app/(tabs)/CategoryPage.tsx
+++ app/(tabs)/CategoryPage.tsx
@@ -2,6 +2,16 @@ import { useState } from 'react';
 import { View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { Text } from '@/components/ui/text';
 import { useCategoryStore } from '@/store/useCategoryStore';
@@ -22,24 +32,71 @@ export default function CategoryPage() {
   return (
     <ScrollView>
       <View className='flex w-full flex-1 flex-col gap-2 px-8'>
+        <>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant='outline'>
+                <Text>Edit Profile</Text>
+              </Button>
+            </DialogTrigger>
+            <DialogContent className='sm:max-w-[425px]'>
+              <DialogHeader>
+                <DialogTitle>Edit profile</DialogTitle>
+                <DialogDescription>
+                  Make changes to your profile here. Click save when you're
+                  done.
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <DialogClose asChild>
+                  <Button>
+                    <Text>OK</Text>
+                  </Button>
+                </DialogClose>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </>
         {categories.map((c) => {
           return (
             <View className='flex flex-row justify-between gap-2' key={c.id}>
               <Text>{c.name}</Text>
-              <Button
-                variant={'destructive'}
-                onPress={async () => {
-                  try {
-                    setIoInProgress(true);
-                    await removeCategory(c.id);
-                  } finally {
-                    setIoInProgress(false);
-                  }
-                }}
-                disabled={ioInProgress}
-              >
-                <Text>Delete</Text>
-              </Button>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button variant='outline' disabled={ioInProgress}>
+                    <Text>Delete</Text>
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className='sm:max-w-[425px]'>
+                  <DialogHeader>
+                    <DialogTitle>Confirm Delete</DialogTitle>
+                    <DialogDescription>
+                      Are you sure you want to delete this category? This action
+                      cannot be undone.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <View className='flex flex-row justify-end'>
+                        <Button
+                          variant={'destructive'}
+                          onPress={async () => {
+                            try {
+                              setIoInProgress(true);
+                              await removeCategory(c.id);
+                            } finally {
+                              setIoInProgress(false);
+                            }
+                          }}
+                          disabled={ioInProgress}
+                        >
+                          <Text>Yes</Text>
+                        </Button>
+                      </View>
+                    </DialogClose>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
             </View>
           );
         })}
